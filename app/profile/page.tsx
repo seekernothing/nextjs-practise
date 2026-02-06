7b97b7ecdc0947981824b2df8f353e14a68fb117
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2026-02-06 17:44:39 +0530

    useRouter,usePathname,useSearchParams

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,28 +1,31 @@
-"use client"
+"use client";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-import { useRouter } from 'next/navigation'
+import React from "react";
 
-import React from 'react'
+const Page = () => {
+  const router = useRouter();
+  const pathName = usePathname();
+  const searchParams = useSearchParams()
 
-const page = () => {
-    const router = useRouter()
+  console.log(
+    pathName,
+    searchParams.getAll("name"),
+    searchParams.has("age")
+  );
+  
 
-    const handelNavigation = ()=>{
-
-        router.push("/")
-    }
+  const handelNavigation = () => {
+    router.push("/");
+  };
   return (
     <div>
-        <h1> Profile component - client component</h1>
+      <h1> Profile component - client component</h1>
 
-        <button
-        onClick={handelNavigation}
-        >
-            Navigate to home page
-        </button>
+      <button onClick={handelNavigation}>Navigate to home page</button>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
